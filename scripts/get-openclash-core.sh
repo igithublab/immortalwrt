8167edee83d129014db0d68197042ed3a97f0335
igithublab <66892031+igithublab@users.noreply.github.com>
2021-03-28 19:02:16 +0800

    Update get-openclash-core.sh

--- scripts/get-openclash-core.sh
+++ scripts/get-openclash-core.sh
@@ -5,15 +5,15 @@ then
 fi
 # Add openclash
 
-cd /workdir/openwrt
-mkdir -p files/etc/openclash/core
+mkdir -p /workdir/openwrt/files/etc/openclash/core
+cd /workdir/openwrt/files/etc/openclash/core
 
 clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
 clash_tun_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
 clash_game_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
 
-wget -qO- $clash_main_url | gunzip -c > files/etc/openclash/core/clash
-wget -qO- $clash_tun_url  | gunzip -c > files/etc/openclash/core/clash_tun
-wget -qO- $clash_game_url | tar xOvz > files/etc/openclash/core/clash_game
+wget $clash_main_url && tar zxvf clash-linux-*.gz && rm -f clash-linux-*.gz
+wget -qO- $clash_tun_url  | gunzip -c > clash_tun
+wget -qO- $clash_game_url | tar xOvz > clash_game
 
-chmod +x files/etc/openclash/core/clash*
+chmod +x clash*
